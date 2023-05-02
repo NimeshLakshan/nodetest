d3b0208c84e84c07623728a21426fdc5fac720f2
NimeshLakshan <71558479+NimeshLakshan@users.noreply.github.com>
2023-05-02 08:15:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+// Set up CORS headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any origin to access the server
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE"); // Allow specific HTTP methods
+  res.header("Access-Control-Allow-Headers", "Content-Type"); // Allow specific headers
+  next();
+});
+
 app.get('/', (req, res) => {
     // Retrieve the data you want to send to the Arduino
     let data = 'Hello, world!';
@@ -18,5 +26,5 @@ app.get('/data', (req, res) => {
 });
 
 app.listen(5000, () => {
-    console.log('Server listening on port 3000');
+    console.log('Server listening on port 5000');
 });
