569fad0ea2a4091b3d1901d7713290605d43ed6b
NimeshLakshan <71558479+NimeshLakshan@users.noreply.github.com>
2023-05-04 19:08:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
